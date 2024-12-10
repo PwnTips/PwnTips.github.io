939f92f0fe606e28e4f74787c26f25107c0a0abe
PwnTips <J.Doe1996@protonmail.com>
2024-12-10 18:36:09 +0800

    add pub.ps1 for convient publish

---
+++ pub.ps1
@@ -0,0 +1,8 @@
+$env:JEKYLL_ENV='production'
+bundle exec jekyll build
+git checkout master
+cp -r -Force _site/* .
+rm -r -Force _site/
+touch .nojekyll
+git add .
+git commit -m 'publish new post'
